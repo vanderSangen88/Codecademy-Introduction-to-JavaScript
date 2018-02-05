99b6dfdb4c79b6e8a117bb09042bc30317faa30f
nickvandersangen <nick.vandersangen@student.fontys.nl>
2018-02-05 22:15:32 +0100

    Setting up folder structure & adding first chapters

---
+++ excercises/1-Introduction/1-1-Introduction-to-JavaScript/1-1-3-Data-Types/app.js
@@ -0,0 +1,27 @@
+/**
+ * 1. On line 1, log the string 'JavaScript' to the console.
+ */
+
+console.log('JavaScript');
+
+
+/**
+ * 2. On line 2, log the number 33.7 to the console.
+ */
+
+console.log(33.7);
+
+
+/**
+ * 3. On line 3, print the boolean true to the console.
+ */
+
+console.log(true);
+
+
+/**
+ * 4. On line 4, print null to the console.
+ */
+
+console.log(null);
+
